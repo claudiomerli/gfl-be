a985f22c5841596ad722c1aeb93357328cf9b4cd
Claudio Merli <cladlomerli99@gmail.com>
2021-08-16 13:20:15 +0200

    added interface to principal

--- src/main/java/it/xtreamdev/gflbe/security/model/JwtUserPrincipal.java
+++ src/main/java/it/xtreamdev/gflbe/security/model/JwtUserPrincipal.java
@@ -4,8 +4,9 @@ package it.xtreamdev.gflbe.security.model;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import it.xtreamdev.gflbe.model.User;
+import org.springframework.security.core.AuthenticatedPrincipal;
 
-public class JwtUserPrincipal {
+public class JwtUserPrincipal implements AuthenticatedPrincipal {
 
     private final Jws<Claims> jwsClaims;
     private final User user;
@@ -17,5 +18,10 @@ public class JwtUserPrincipal {
 
     public User getUser() {
         return user;
+    }
+
+    @Override
+    public String getName() {
+        return this.getUser().getUsername();
     }
 }
