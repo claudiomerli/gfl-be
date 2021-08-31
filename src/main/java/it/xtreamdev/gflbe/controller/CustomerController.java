6e79639d0e7cac15e14da0397d0fe8115fd0ed55
Alessio Canestrelli <a.canestrelli@gmail.com>
2021-08-31 11:21:41 +0200

    crud customer

--- src/main/java/it/xtreamdev/gflbe/controller/CustomerController.java
+++ src/main/java/it/xtreamdev/gflbe/controller/CustomerController.java
@@ -1,10 +1,8 @@
 package it.xtreamdev.gflbe.controller;
 
 import it.xtreamdev.gflbe.dto.SaveCustomerRestDTO;
-import it.xtreamdev.gflbe.exception.GLFException;
 import it.xtreamdev.gflbe.model.Customer;
-import it.xtreamdev.gflbe.repository.ContentRulesRepository;
-import it.xtreamdev.gflbe.repository.CustomerRepository;
+import it.xtreamdev.gflbe.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,26 +14,41 @@ import java.util.List;
 @RestController
 @RequestMapping("api/customer")
 public class CustomerController {
-    private final CustomerRepository customerRepository;
-    private final ContentRulesRepository contentRulesRepository;
+    private final CustomerService customerService;
 
     @GetMapping
-    public ResponseEntity<List<Customer>> findAll() {
+    public ResponseEntity<List<Customer>> search(
+            @RequestParam(value = "globalSearch") String globalSearch
+    ) {
         return ResponseEntity.ok(
-                customerRepository.findAll()
+                customerService.search(globalSearch)
+        );
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Customer> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok(
+                customerService.findById(id)
         );
     }
 
     @PostMapping
     public ResponseEntity<Customer> save(@RequestBody SaveCustomerRestDTO saveCustomerRestDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(
-                customerRepository.save(Customer.builder()
-                        .name(saveCustomerRestDTO.getName())
-                        .contentRules(
-                                contentRulesRepository.findById(saveCustomerRestDTO.getContentRulesId())
-                                        .orElseThrow(() -> new GLFException("customer not found", HttpStatus.UNPROCESSABLE_ENTITY))
-                        )
-                        .build())
+                customerService.save(saveCustomerRestDTO)
         );
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Customer> update(@PathVariable Integer id, @RequestBody SaveCustomerRestDTO saveCustomerRestDTO) {
+        return ResponseEntity.ok(
+                customerService.update(id, saveCustomerRestDTO)
+        );
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        customerService.delete(id);
+        return ResponseEntity.ok().build();
     }
 }
