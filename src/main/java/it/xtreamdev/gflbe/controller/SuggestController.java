ebf07393a4791bdad46aa290f865275f7b0c9ea8
giacintotucciarone@gmail.com <Giacinto1>
2021-09-08 09:53:31 +0200

    get and delete suggests

--- src/main/java/it/xtreamdev/gflbe/controller/SuggestController.java
+++ src/main/java/it/xtreamdev/gflbe/controller/SuggestController.java
@@ -1,14 +1,14 @@
 package it.xtreamdev.gflbe.controller;
 
+import it.xtreamdev.gflbe.dto.SuggestsDTO;
 import it.xtreamdev.gflbe.dto.RequestSuggest;
 import it.xtreamdev.gflbe.service.SuggestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/suggests")
@@ -22,6 +22,17 @@ public class SuggestController {
     public ResponseEntity<Void> create(@RequestBody RequestSuggest requestSuggest){
         suggestService.saveKeyWord(requestSuggest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SuggestsDTO>> findAll() {
+        return ResponseEntity.ok(suggestService.getAll());
+    }
+
+    @DeleteMapping(value = "/{idSuggest}")
+    public ResponseEntity<Void> deleteById(@PathVariable(value = "idSuggest") String idSuggest) {
+        suggestService.deleteById(idSuggest);
+        return ResponseEntity.ok().build();
     }
 
 }
